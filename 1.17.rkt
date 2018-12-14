5e2aff675c3901822f01ef04a2364da75df2fb2e
dustandash <35522699+dustandash@users.noreply.github.com>
2018-12-15 01:44:17 +0800

    Create 1.17.rkt

---
+++ 1.17.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(define (double n)
+  (* 2 n))
+(define (halve n)
+  (cond ((= (remainder n 2) 0)(/ n 2))))
+(define (multi a b)
+    (cond ((= b 0)
+            0)
+          ((even? b)
+            (double (multi a (halve b))))
+          ((odd? b)
+            (+ a (multi a (- b 1))))))
